ac864634d5af85c6b165876ba7d11cb231f640bc
Shiping Yi <esp132@gmail.com>
2018-02-19 17:31:01 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,13 +1,4 @@
-def display_board(board)
-  puts"Welcome to Tic Tac Toe!"
-  puts"where would you like to go?"
-  input=input_to_index(gets.strip)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+
 
 # code your input_to_index and move method here!
 def input_to_index(string)
@@ -15,5 +6,14 @@ int=string.to_i-1
 end
 
 def move(board,int,symbol="X")
+
 board[int]=symbol
+end
+def display_board(board)
+
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
