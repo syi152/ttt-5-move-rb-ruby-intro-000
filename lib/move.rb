20ce12b2f51c85656089def0c30a8a2cbe2f9341
Shiping Yi <esp132@gmail.com>
2018-02-19 16:41:44 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,6 @@
 def display_board(board)
+  puts"where would you like to move?"
+  input=gets.strip.to_i
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
